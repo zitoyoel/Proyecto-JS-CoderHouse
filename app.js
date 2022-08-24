0f3d716261e714e01e4362fcacf6ee10d35728be
Yoel Zito <yzito@axoft.com>
2022-08-24 16:41:37 -0300

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,245 @@
+/* En esta nueva entrega se cambia la seleccion del horario del turno, con nuevas validaciones que no dejan sacar turnos en el mismo dia y a la misma hora.
+Tambien se agrega un boton en el nav para consultar los turnos y se muestran de forma dinamica. Se agrega boton que permite eliminar los turnos (se eliminan dinamicamente en el html y en el storage). */
+
+class Turno {
+    constructor(nombre, apellido, dni, telefono, hora, fecha) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.hora = hora;
+        this.fecha = fecha;
+    }
+
+    datosUsuario() {
+        return `Nombre: ${this.nombre}\nApellido: ${this.apellido}\nDNI: ${this.dni}\nTelefono: ${this.telefono}\nHora: ${this.hora} hs\nFecha: ${this.fecha}`;
+    }
+
+}
+
+
+const turnos = [];
+console.log(turnos);
+verificarStorage();
+
+//interactuar con html
+
+
+//esconder boton de sacar turno
+let esconderBoton = document.getElementById("botonTurno");
+esconderBoton.onclick = () => {
+    esconderBoton.style.display = "none";
+};
+
+//al hacer click en sacar turno, aparece el formulario
+let botonReservar = document.getElementById("botonTurno");
+botonReservar.addEventListener("click", formularioAparece);
+
+function formularioAparece() {
+    let formularioDespliega = document.getElementById("formularioAparece");
+    formularioDespliega.style.display = "flex";
+}
+
+//creo variable para despues desplegar el formulario
+let formularioDespliega = document.getElementById("formularioAparece");
+
+//funcionalidad para el boton cerrar del formulario
+let botonCerrar = document.getElementById("cerrarFormulario");
+botonCerrar.onclick = () => {
+    formularioDespliega.style.display = "none";
+    esconderBoton.style.display = "block";
+};
+
+//funcionalidad mostrar turnos
+//Si no hay turnos registrados, se muestra un mensaje
+let botonMostrarTurnos = document.getElementById("misTurnos");
+botonMostrarTurnos.onclick = () => {
+    let auxiliar = JSON.parse(localStorage.getItem("turnos"));
+    if(turnos.length === 0 || auxiliar === null){
+        let sinTurnos = document.getElementById("infoModal");
+        sinTurnos.innerHTML = "NO HAY TURNOS REGISTRADOS";
+        
+    }
+    //si hay turnos registrados, se muestran los turnos de forma dinamica y se pueden eliminar
+    else{
+        let tabla = document.getElementById("infoModal");
+        tabla.innerHTML = "";
+        for(let i = 0; i < auxiliar.length; i++){
+            tabla.innerHTML += `<div class="registrosTabla">
+            <h5 class="titulosTurnos">Nombre</h5>
+            <p class="datosTurnos">${auxiliar[i].nombre}</p>
+            <h5 class="titulosTurnos">Apellido</h5>
+            <p class="datosTurnos">${auxiliar[i].apellido}</p>
+            <h5 class="titulosTurnos">DNI</h5>
+            <p class="datosTurnos">${auxiliar[i].dni}</p>
+            <h5 class="titulosTurnos">Telefono</h5>
+            <p class="datosTurnos">${auxiliar[i].telefono}</p>
+            <h5 class="titulosTurnos">Horario</h5>
+            <p class="datosTurnos">${auxiliar[i].hora}</p>
+            <h5 class="titulosTurnos">Fecha</h5>
+            <p class="datosTurnos">${auxiliar[i].fecha}</p>
+            </div>`;    
+        }
+        
+            
+            
+            
+        }
+
+    } 
+    
+//funcionalidad eliminar turnos
+let eliminar = document.getElementById("eliminarTurnos");
+eliminar.onclick = () => {
+guardarStorage();    
+localStorage.clear();
+turnos.splice(0, turnos.length);
+}
+
+
+//interaccion con furmulario
+window.addEventListener('load', () => {
+    const form = document.getElementById('formulario')
+    const nombre = document.getElementById("inputNombre")
+    const apellido = document.getElementById("inputApellido")
+    const dni = document.getElementById("inputDNI")
+    const fecha = document.getElementById("inputFecha")
+    const hora = document.getElementById("inputHorario")
+    const telefono = document.getElementById("inputTelefono");
+    
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if(validaCampos() === true){
+            
+            let turno = new Turno(nombre.value, apellido.value, dni.value, telefono.value, hora.value, fecha.value);
+            turno.datosUsuario();
+            console.log(`Los datos del turno son:\n${turno.datosUsuario()}`);
+            
+            Swal.fire({
+                title: 'DETALLE DE SU TURNO',
+                html: `<p>Nombre: ${turno.nombre}</p><p>Apellido: ${turno.apellido}</p><p>DNI: ${turno.dni}</p><p>Telefono: ${turno.telefono}</p><p>Hora: ${turno.hora}</p><p>Fecha: ${turno.fecha}</p>`,
+                icon: 'success',
+                confirmButtonText: 'Cerrar'
+            })
+            
+            turnos.push(turno);
+            guardarStorage();
+            console.log(`El usuario saco ${turnos.length} turnos`);
+            formulario.reset();
+            formularioDespliega.style.display = "none";
+            esconderBoton.style.display = "block";
+
+            
+        } 
+    })
+    
+    
+    const validaCampos = () => {
+        //capturar los valores ingresados por el usuario
+        const nombreValor = nombre.value.trim()
+        const apellidoValor = apellido.value.trim()
+        const dniValor = dni.value.trim()
+        const horaValor = hora.value;
+        const telefonoValor = telefono.value.trim();
+        const fechaValor = fecha.value;
+        
+        
+        //validando campo nombre
+        if (!nombreValor) {
+            validaFalla(nombre, 'Campo vacío')
+            return false
+        } else {
+            validaOk(nombre)
+        }
+        
+        //validando campo apellido
+        if (!apellidoValor) {
+            validaFalla(apellido, 'Campo vacío')
+            return false
+        } else {
+            validaOk(apellido)
+        }
+        
+        //validando campo DNI
+        if (!dniValor) {
+            validaFalla(dni, 'Campo vacío')
+            return false
+        } else {
+            validaOk(dni)
+        }
+        
+        //validando campo telefono
+        if (!telefonoValor) {
+            validaFalla(telefono, 'Campo vacío')
+            return false
+        } else {
+            validaOk(telefono)
+        }
+        
+        //validando campo fecha
+        if (!fechaValor) {
+            validaFalla(fecha, 'Campo vacío')
+            return false
+        } else if (new Date(fechaValor) < new Date()) {
+            validaFalla(fecha, 'Ingrese una fecha mayor a la actual')
+            return false
+        } else if(new Date(fechaValor).getDay() === 5 || new Date(fechaValor).getDay() === 6){
+            validaFalla(fecha, 'No atendemos los fines de semana')
+            return false
+        } else {
+            validaOk(fecha)
+        }
+        
+        //validando campo hora
+        
+        if (!horaValor) {
+            validaFalla(hora, 'Campo vacío')
+            return false
+        }
+        else if(horaValor){
+            for(let i = 0; i < turnos.length; i++){
+                if(turnos[i].hora === horaValor && turnos[i].fecha === fechaValor){
+                    validaFalla(hora, 'Turno ocupado, seleccione otro horario')
+                    return false
+                }
+    
+            }
+        } else {
+            validaOk(hora)
+        }
+        
+        
+        return true;
+        
+    }
+})
+
+const validaFalla = (input, msje) => {
+    const formControl = input.parentElement
+    const aviso = formControl.querySelector('p')
+    aviso.innerText = msje
+    
+    formControl.className = 'form-control falla'
+}
+const validaOk = (input, msje) => {
+    const formControl = input.parentElement
+    formControl.className = 'form-control ok'
+}
+
+//Storage
+
+function guardarStorage(){
+    localStorage.setItem("turnos", JSON.stringify(turnos));
+}
+
+function verificarStorage(){
+    let auxiliar = JSON.parse(localStorage.getItem("turnos"));
+
+    if(auxiliar){
+        for(let i = 0; i < auxiliar.length; i++){
+            turnos.push(auxiliar[i]);
+        }
+    }
+    
+}
